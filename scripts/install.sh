d8bff96e15a7a69fada6aba8a146d8bf6e402181
BizzyPythonBear <bizzeebear@hotmail.com>
2021-08-24 21:10:26 -0700

    Website Added

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ sudo mandb
 cd ../
 
 printf "\033c"
-printf "\033[0;92m"
+printf "\033[0;32m"
 echo "Enjoy Bear Package Manager!"
 echo "'bpm install <package>' should be ran with sudo."
 echo "Run 'bpm --help' to view all the commands!"
